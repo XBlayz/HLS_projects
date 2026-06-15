6c1dc96ba208203c65625b809a9f7e7ebefdd177
Stefano Scarcelli <stefano.scarcelli15@gmail.com>
2026-06-15 10:26:31 +0200

    feat(spmv): add pipeline L1 SpMV implementation

---
+++ projects/project02_SpMV/02_pipeline-L1/spmv.cpp
@@ -0,0 +1,28 @@
+#include "spmv.hpp"
+
+void spmv(
+    const mtx_data_t values[NNZ], const vec_data_t x[NCOLS],
+    const int col_idx[NNZ], const int row_ptr[NROWS + 1],
+    out_data_t y[NROWS]
+) {
+    // Iterate over rows
+    L1: for (int row = 0; row < NROWS; row++) {
+        //* OPTIMIZATION(Pipeline): running SpMV in pipeline
+        #pragma HLS PIPELINE II=1
+
+        out_data_t sum = 0.0f;
+
+        // Iterate over non-zero elements
+        L2: for (int idx = row_ptr[row]; idx < row_ptr[row + 1]; idx++) {
+            //* MIN: at lest 1 non-zero element
+            //* MAX: 128 non-zero elements (entire row)
+            //* AVG: 4 non-zero elements (typically 512 non-zero elements i total, 512/128 = 4 per row)
+            #pragma HLS loop_tripcount min=1 max=128 avg=4
+
+            // --- MAC ---
+            sum += values[idx] * x[col_idx[idx]];
+        }
+
+        y[row] = sum;
+    }
+}
